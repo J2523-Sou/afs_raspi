c31c59be01bf68f937d48cb62473e64e838517ca
daihachi10 <134046242+daihachi10@users.noreply.github.com>
2026-07-08 15:26:31 +0900

    スピードを変更

--- yamaguchi.py
+++ yamaguchi.py
@@ -67,9 +67,9 @@ def _motor_from_buttons(forward: bool, reverse: bool) -> Tuple[int, int]:
     - 0: 後退
     """
     if forward:
-        return 150, DIR_FORWARD
+        return 200, DIR_FORWARD
     if reverse:
-        return 150, DIR_REVERSE
+        return 200, DIR_REVERSE
     return 0, 0
 
 
