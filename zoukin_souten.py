da80bcffa098facdcf1913f7844ff4be1269144b
2026B-raspi <2026B-raspi@mrp.com>
2026-09-17 12:43:50 +0900

    シリンダーの動作を適応

--- zoukin_souten.py
+++ zoukin_souten.py
@@ -60,13 +60,31 @@ LIMIT2_PIN = 12 # 左側のリミットスイッチ
 LIMIT3_PIN = 9  # サーボの先のリミットスイッチ
 LIMIT_RESEAT_TIMEOUT = 3.0
 
-SOUTEN_KIKOU_ICHI = "NULL"  # 雑巾装填機構の位置を示す（右,左） まだプログラムに追加してないので後から絶対に追加する.
+HIDARI_CYLINDER_FIRE_PARMISSION = False
+MIGI_CYLINDER_FIRE_PARMISSION = False
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LIMIT1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LIMIT2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LIMIT3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+def fire_cylinder_check():
+    "それぞれのエアシリンダが発射可能状態かどうかチェックする"
+    global HIDARI_CYLINDER_FIRE_PARMISSION, MIGI_CYLINDER_FIRE_PARMISSION
+
+    if GPIO.input(LIMIT1_PIN) == GPIO.LOW:
+        HIDARI_CYLINDER_FIRE_PARMISSION = True
+    else:
+        HIDARI_CYLINDER_FIRE_PARMISSION = False
+
+    if GPIO.input(LIMIT2_PIN) == GPIO.LOW:
+        MIGI_CYLINDER_FIRE_PARMISSION = True
+    else:
+        MIGI_CYLINDER_FIRE_PARMISSION = False
+
+
+    return HIDARI_CYLINDER_FIRE_PARMISSION, MIGI_CYLINDER_FIRE_PARMISSION
 
 
 def _u8(value: int) -> int:
@@ -113,6 +131,7 @@ def move_until_limit(payload, limit_pin, poll_interval):
 
         # ここに来たらリミットスイッチが押された
         print("[動作完了] リミット到達: pin=", limit_pin)
+        fire_cylinder_check()  # リミットスイッチの状態を更新
         return True
 
     finally:
@@ -343,6 +362,7 @@ def run_zoukin_souten(poll_interval: float = 0.02):
                 continue
 
             emergency_stop_sent = False
+            fire_cylinder_check()
             vals = _get_values()
 
             circle_pressed = _circle_pressed(vals)
@@ -383,4 +403,3 @@ run_receiver = run_zoukin_souten
 
 if __name__ == "__main__":
     run_zoukin_souten()
-
